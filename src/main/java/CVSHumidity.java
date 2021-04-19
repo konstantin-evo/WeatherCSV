7c6deb17c0b926f7cff8e1bd5e055283430c1178
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-04-19 12:57:40 +0300

    Add "public CSVRecord lowestHumidityInFile(CSVParser parser)" method

--- src/main/java/CVSHumidity.java
+++ src/main/java/CVSHumidity.java
@@ -1,2 +1,40 @@
+import edu.duke.FileResource;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
 public class CVSHumidity {
+
+    public CSVRecord getMinimumOfTwo(CSVRecord currentRow, CSVRecord minimumRow) {
+        if (minimumRow == null) {
+            minimumRow = currentRow;
+        } else if (currentRow.get("Humidity") != "N/A"){
+            double currentHumidity = Double.parseDouble(currentRow.get("Humidity"));
+            double minimumHumidity = Double.parseDouble(minimumRow.get("Humidity"));
+
+            if (currentHumidity<minimumHumidity) { minimumRow = currentRow; }
+        }
+        return minimumRow;
+    }
+
+    public CSVRecord lowestHumidityInFile(CSVParser parser){
+        CSVRecord minimumRow = null;
+
+        for (CSVRecord currentRow : parser) {
+            minimumRow = getMinimumOfTwo(currentRow, minimumRow);
+        }
+        return minimumRow;
+    }
+
+    public void testLowestHumidityInFile(){
+        FileResource fr = new FileResource();
+        CSVRecord minimum = lowestHumidityInFile(fr.getCSVParser());
+        System.out.println("Lowest Humidity was " + minimum.get("Humidity") +
+                " at " + minimum.get("DateUTC"));
+    }
+
+    public static void main(String[] args) {
+        CVSHumidity o = new CVSHumidity();
+        o.testLowestHumidityInFile();
+    }
+
 }
