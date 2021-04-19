78ec49b0ab85de15869597f54b9e59ed4258567a
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-04-19 12:26:57 +0300

    Add CSVMin Class - Method fileWithColdestTemperature

--- src/main/java/CSVMin.java
+++ src/main/java/CSVMin.java
@@ -5,17 +5,62 @@ import java.io.*;
 public class CSVMin {
 
     public CSVRecord getMinimumOfTwo(CSVRecord currentRow, CSVRecord minimumRow) {
-        if (minimumRow == null) {//If largestSoFar is nothing
+        if (minimumRow == null) {
             minimumRow = currentRow;
         } else {
             double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
             double minimumTemp = Double.parseDouble(minimumRow.get("TemperatureF"));
 
-            if (currentTemp<minimumTemp) {
-                minimumRow = currentRow;
-            }
+            if (currentTemp != -9999 && currentTemp<minimumTemp) { minimumRow = currentRow; }
         }
         return minimumRow;
+    }
+
+    public CSVRecord minimumHourInFile(CSVParser parser) {
+
+        CSVRecord minimumRow = null;
+
+        for (CSVRecord currentRow : parser) {
+            minimumRow = getMinimumOfTwo(currentRow, minimumRow);
+        }
+        return minimumRow;
+    }
+
+    public void testMinimumInDay () {
+        FileResource fr = new FileResource();
+        CSVRecord minimum = minimumHourInFile(fr.getCSVParser());
+        System.out.println("Minimum temperature was " + minimum.get("TemperatureF") +
+                " at " + minimum.get("TimeEST"));
+    }
+
+    public String fileWithColdestTemperature() {
+        DirectoryResource dr = new DirectoryResource();
+        CSVRecord minimumRow = null;
+        String coldestFileName = null;
+
+        for (File file : dr.selectedFiles()) {
+            FileResource fr = new FileResource(file);
+            CSVRecord currentRow = minimumHourInFile(fr.getCSVParser());
+            minimumRow = getMinimumOfTwo(currentRow, minimumRow);
+            coldestFileName = file.getName();
+        }
+        return coldestFileName;
+    }
+
+    public void testFileWithColdestTemperature() {
+        String coldestname = fileWithColdestTemperature();
+        System.out.println("Coldest day was in file " + coldestname);
+
+        FileResource fr = new FileResource();
+        CSVRecord minimum = minimumHourInFile(fr.getCSVParser());
+        System.out.println("Minimum temperature was " + minimum.get("TemperatureF") +
+                " at " + minimum.get("TimeEST"));
+    }
+
+    public static void main(String[] args) {
+        CSVMin o = new CSVMin();
+        //o.testMinimumInDay();
+        System.out.println(o.fileWithColdestTemperature());
     }
 
 }
