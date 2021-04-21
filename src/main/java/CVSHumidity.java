b6228092d185fb79c6f1dbb23259478c8dd9f93b
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-04-21 16:53:50 +0300

    Add and complete "CVSHumidity" Class

--- src/main/java/CVSHumidity.java
+++ src/main/java/CVSHumidity.java
@@ -1,6 +1,9 @@
+import edu.duke.DirectoryResource;
 import edu.duke.FileResource;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+
+import java.io.File;
 
 public class CVSHumidity {
 
@@ -32,9 +35,28 @@ public class CVSHumidity {
                 " at " + minimum.get("DateUTC"));
     }
 
+    public CSVRecord lowestHumidityInManyFiles() {
+        DirectoryResource dr = new DirectoryResource();
+        CSVRecord lowestHumidity = null; //start with largestSoFar as nothing
+
+        for (File file : dr.selectedFiles()) {
+            FileResource fr = new FileResource(file);
+            CSVRecord currentRow = lowestHumidityInFile(fr.getCSVParser());
+
+            lowestHumidity = getMinimumOfTwo(currentRow, lowestHumidity);
+        }
+        return lowestHumidity;
+    }
+
+    public void testlowestHumidityInManyFiles() {
+        CSVRecord lowestHumidity = lowestHumidityInManyFiles();
+        System.out.println("The lowest Humidity was "+lowestHumidity.get("Humidity")+" at "+lowestHumidity.get("DateUTC"));
+    }
+
     public static void main(String[] args) {
         CVSHumidity o = new CVSHumidity();
-        o.testLowestHumidityInFile();
+        //o.testLowestHumidityInFile();
+        o.testlowestHumidityInManyFiles();
     }
 
 }
